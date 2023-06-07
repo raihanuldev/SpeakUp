a69c678942d5c34fbda8ce9cc8ea980ec0fb2a59
Rihanul Islam <rihanthedev@gmail.com>
2023-06-07 13:57:03 +0600

    SingUp page Design

---
+++ src/Pages/Shared/Singup/Singup.jsx
@@ -0,0 +1,85 @@
+import { useState } from "react";
+import { useForm } from "react-hook-form";
+import { Link } from "react-router-dom";
+
+const Singup = () => {
+    const { register, handleSubmit, watch, formState: { errors } } = useForm();
+    const [showPassword, setShowPassword] = useState(false);
+    const handleshowPassword = () => {
+        setShowPassword(!showPassword)
+    }
+    const onSubmit = data => console.log(data);
+
+    return (
+        <div className="hero min-h-screen bg-base-200">
+            <div className="hero-content flex-col lg:flex-row-reverse">
+                <div className="text-center lg:text-left">
+                    <h1 className="text-5xl font-bold">SingUp now!</h1>
+                    <p className="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
+                </div>
+                <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+                    <form onSubmit={handleSubmit(onSubmit)} className="card-body">
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">Name</span>
+                            </label>
+                            <input {...register("name",{ required: true })} type="text" placeholder="Name" className="input input-bordered" />
+                        </div>
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">Photo Url</span>
+                            </label>
+                            <input {...register("photo",{ required: true })} type="text" placeholder="Paste URL" className="input input-bordered" />
+                        </div>
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">Email</span>
+                            </label>
+                            <input {...register("email", { required: true })} type="text" placeholder="email" className="input input-bordered" />
+                        </div>
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">Password</span>
+                            </label>
+                            <input {...register("password", {
+                                required: true,
+                                minLength: 6,
+                                pattern: {
+                                    value: /^(?=.*[A-Z])(?=.*[@#$%^&+=]).*$/,
+                                    message: 'Password must contain at least one capital letter and one special character',
+                                },
+                            })}
+                                placeholder="password" className="input input-bordered" />
+                            {errors.password && <span>{errors.password.message}</span>}
+                            {errors.password?.type === 'minLength' && (
+                                <span>Password must be at least 6 characters long</span>
+                            )}
+
+                            <label className="label">
+                                <span className="label-text">Confirm Password</span>
+                            </label>
+                            <input {...register('confirmPassword', {
+                                required: true,
+                                validate: (value) => value === watch('password') || 'Passwords do not match',
+                            })} placeholder="password" className="input input-bordered" />
+                            {errors.confirmPassword && <span>{errors.confirmPassword.message}</span>}
+
+                            <div className="flex justify-between">
+                                <label className="label">
+                                    <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
+                                </label>
+                                <label className="label">
+                                    <Link to='/singup' className="label-text-alt link link-hover">Are You New? Singup</Link>
+                                </label>
+                            </div>
+                        </div>
+                        <input className="btn btn-primary" type="submit" value="SIngUp" />
+
+                    </form>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Singup;
--- src/Routes/router.jsx
+++ src/Routes/router.jsx
@@ -2,6 +2,7 @@ import { createBrowserRouter } from "react-router-dom";
 import Main from "../Layouts/Main";
 import Home from '../pages/Home/Home/Home'
 import Login from "../Pages/Shared/Login/Login";
+import Singup from "../Pages/Shared/Singup/Singup";
 
 export const router = createBrowserRouter([
     {
@@ -18,5 +19,9 @@ export const router = createBrowserRouter([
     {
         path:'/login',
         element: <Login></Login>
+    },
+    {
+        path:'/singup',
+        element: <Singup></Singup>
     }
 ])
