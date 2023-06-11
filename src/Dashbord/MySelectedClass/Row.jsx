a6a604b1d40c611906b3c1a155a9b28301ce110d
Rihanul Islam <rihanthedev@gmail.com>
2023-06-12 05:30:19 +0600

    Handle Remove

--- src/Dashbord/MySelectedClass/Row.jsx
+++ src/Dashbord/MySelectedClass/Row.jsx
@@ -1,11 +1,40 @@
 import React from 'react';
 import { Link, useNavigate } from 'react-router-dom';
+import {FaTrashAlt} from 'react-icons/fa'
+import Swal from 'sweetalert2';
+import UseCart from '../../Hooks/UseCart';
 
 const Row = ({ row, index }) => {
+    const [,refetch] = UseCart()
     const navigate = useNavigate()
     const handlePayNow = ()=>{
         localStorage.setItem('row',JSON.stringify(row))
         navigate('/dashbord/payment', {state:row})
+    }
+    // handle Remove Area
+    const handleRemove =()=>{
+        const couresId = {cartId:row.cartId}
+        console.log(couresId);
+        fetch('http://localhost:5000/carts',{
+            method: "DELETE",
+            headers: {
+                'content-type':'application/json'
+            },
+            body: JSON.stringify(couresId)
+        })
+        .then(res=>res.json())
+        .then(data=>{
+            if(data.deletedCount>0){
+                refetch()
+                Swal.fire({
+                    position: 'top-end',
+                    icon: 'success',
+                    title: 'Item Remove On Cart!!',
+                    showConfirmButton: false,
+                    timer: 2500
+                  })
+            }
+        })
     }
 
     return (
@@ -30,7 +59,7 @@ const Row = ({ row, index }) => {
                 <p className='text-xl'>${row.price}</p>
             </td>
             <td>
-                <button className='btn btn-ghost '>Remove</button>
+                <button onClick={handleRemove} className='btn btn-ghost '> <FaTrashAlt></FaTrashAlt> </button>
             </td>
             <th>
                 <Link to={{ pathname: '/dashbord/payment', state: { row } }}><button onClick={handlePayNow} className="btn btn-ghost">Pay Now</button></Link>
