46c80b6b4945b3acbb88bbdb1747f02a50d87b82
uniquecoderrihan <rihanthedev@gmail.com>
2023-09-25 00:55:17 +0600

    Community

---
+++ src/Pages/commingPage/CommingPage.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const CommingPage = () => {
+    return (
+        <img className='w-full' src="https://i.ibb.co/GvHp4wD/comming.png" alt="" />
+    );
+};
+
+export default CommingPage;
---
+++ src/Pages/community/Community.jsx
@@ -0,0 +1,11 @@
+import CommingPage from "../commingPage/CommingPage";
+
+const Community = () => {
+    return (
+        <div>
+            <CommingPage/>
+        </div>
+    );
+};
+
+export default Community;
