54f3cfdbd5f961227c7983a09d1d82ec808260fa
raihanuldev <rihanthedev@gmail.com>
2024-04-16 00:53:11 +0600

    Mentors Card Demo added

--- src/Pages/Instructors/Instructors.jsx
+++ src/Pages/Instructors/Instructors.jsx
@@ -4,7 +4,6 @@ import { useQuery } from "@tanstack/react-query";
 import { Helmet } from "react-helmet-async";
 
 const Instructors = () => {
-  // const [instructors, setInstructor] = useState([])
   const { data: instructor = [] } = useQuery({
     queryKey: "instructor",
     queryFn: async () => {
@@ -16,12 +15,22 @@ const Instructors = () => {
     <div className="mt-[6rem]">
       <div>
         <Helmet>
-          <title>Instructors || SpeakUp</title>
+          <title>Mentors || SpeakUp</title>
         </Helmet>
       </div>
       <div>
         <h1 className="text-3xl text-center font-semibold">Mentors Team </h1>
-        {/* <h1 className="text-3xl">Mentors Team </h1> */}
+        <div className="card w-96 bg-base-200 shadow-xl flex flex-col items-center p-5 transform transition-all duration-1000 hover:scale-105">
+          <div className="avatar flex items-center justify-center ">
+            <div className="w-24 rounded-full ring ring-primary ring-offset-base-100 ring-offset-2">
+              <img src="https://daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg" />
+            </div>
+          </div>
+          <div className="card-body text-center">
+            <h2 className="card-title">Raihanul Islam</h2>
+            <h2>senior web Mentor</h2>
+          </div>
+        </div>
       </div>
     </div>
   );
